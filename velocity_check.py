57616de59edc0b2794ad6648165d4bbe7b0909db
SumedhP <43325361+SumedhP@users.noreply.github.com>
2024-07-08 20:48:08 -0700

    this should work

--- velocity_check.py
+++ velocity_check.py
@@ -1,6 +1,7 @@
 from csv_parser import read_data, get_data
 from os import listdir
 import matplotlib.pyplot as plt
+import statistics
 
 DATA_FOLDER = "data"
 
@@ -22,6 +23,7 @@ def main():
     data = read_data(DATA_FOLDER + "/" + file)
     accel_x, accel_y, of_x, of_y, heading = get_data(data)
     plot_data(of_x, of_y)
+    # average_velocity(of_x, of_y)
 
 def plot_data(vx, vy):
   plt.figure("Velocity")
@@ -30,4 +32,12 @@ def plot_data(vx, vy):
   plt.legend(["X", "Y"])
   plt.show()
 
+def average_velocity(vx, vy):
+  x = list()
+  y = list()
+  for i in range(DATA_START, DATA_END):
+    x.append(vx[i])
+    y.append(vy[i])
 
+  print("X: " + str(statistics.mean(x)))
+  print("Y: " + str(statistics.mean(y)))
